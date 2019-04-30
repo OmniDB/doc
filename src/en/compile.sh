7c3f9fced49ddf91ac5c98ebbc15ef3ba6175800
William Ivanski <william.ivanski@gmail.com>
2019-04-30 14:55:58 -0300

    Removed Eisvogel

--- src/en/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Put file misc/eisvogel.latex in ~/.pandoc/templates/
-# Credits: https://github.com/Wandmalfarbe/pandoc-latex-template
-
-pandoc *.md -o omnidb_handbook.pdf --toc --template eisvogel --listings --top-level-division=chapter -V lang=en-US -V titlepage=true -V toc-own-page=true -V logo=../../misc/omnidb_logo.png -V logo-width=240 -V book -V classoption=oneside -V listings-disable-line-numbers=true
